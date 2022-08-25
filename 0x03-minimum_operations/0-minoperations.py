a1f59c63c68907d9de5c8a64328e980a57738c82
Julian <jose.torres122@outlook.com>
2022-08-24 23:07:35 -0400

    A functions that calculates the min # of operations to reach n amount of 'H's by copy and pasting.

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Module contains minOperations function'''
+
+
+def minOperations(n):
+    '''
+    Function calculates the fewest bufferber of operations 
+    needed to result in exactly n 'H' characters in the file
+    '''
+    buffer = n
+    div = 2
+    iterations = 0
+    if type(n) is not int or n < 2:
+        return 0
+    while buffer > 1:
+        if buffer % div == 0:
+            buffer /= div
+            iterations += div
+        else:
+            div += 1
+    return iterations

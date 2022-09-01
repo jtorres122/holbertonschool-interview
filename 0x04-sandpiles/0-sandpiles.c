06a6746c5bad43301d4f30a3350965fcf95892ff
Julian <jose.torres122@outlook.com>
2022-09-01 10:44:10 -0400

    Sandpiles setup

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,10 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - function computes the sum of two sandpiles
+ * @grid1: sandpile 1
+ * @grid2: sandpile 2
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+}

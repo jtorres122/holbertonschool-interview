95166c663b5c5c1d79dd925881a84e75e3395785
Julian <jose.torres122@outlook.com>
2022-09-08 20:15:23 -0400

    Be a function that reverses a singly linked list and then determines if it is a palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,4 +9,51 @@
  */
 int is_palindrome(listint_t **head)
 {
+	int length, center, idx;
+	int validator = 1;
+	listint_t *head_rev;
+	listint_t *actual = *head;
+	listint_t *counter = *head;
+	listint_t *reversed_actual;
+
+	for (length = 0; counter != NULL; length++)
+		counter = counter->next;
+	center = length / 2;
+
+	head_rev = *head;
+	for (idx = 0; idx < center; idx++)
+		head_rev = head_rev->next;
+	if (length % 2 != 0)
+		head_rev = head_rev->next;
+	head_rev = reverse_list(&head_rev);
+	reversed_actual = head_rev;
+	actual = *head;
+	while (reversed_actual != NULL)
+	{
+		if (reversed_actual->n != actual->n)
+			validator = 0;
+		reversed_actual = reversed_actual->next;
+		actual = actual->next;
+	}
+	return (validator);
+}
+
+/**
+ * reverse_list - function reverses singly linked list
+ * @head: list to be reversed
+ * Return: the reversed list
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *actual = *head, *next = NULL;
+
+	while (actual != NULL)
+	{
+		next = actual->next;
+		actual->next = prev;
+		prev = actual;
+		actual = next;
+	}
+	*head = prev;
+	return (*head);
 }
